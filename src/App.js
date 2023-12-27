1124e7ae31405d35d6961effe176e2d2d6b3cbe3
Italo Marcelo <MarceloDev2000@outlook.com>
2023-12-27 19:49:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Nav from './components/navbar/MenuNavBar'
+import Main from './components/main/Main'
 import './App.css';
+
+import icon from './components/img/logo.svg'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Nav logo={icon}   />
+         <Main />
     </div>
   );
 }
